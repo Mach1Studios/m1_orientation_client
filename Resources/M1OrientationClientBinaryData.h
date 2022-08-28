91349df5abee6195b64415e00458e37772c532c1
Dylan Marcus <dylan@mach1studios.com>
2022-08-28 22:38:44 +0100

    remove ui and add binary data class

---
+++ Resources/M1OrientationClientBinaryData.h
@@ -0,0 +1,16 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace M1OrientationClientBinaryData
+{
+    extern const char*  tech_support_icon_png;
+    const int           tech_support_icon_pngSize = 18979;
+
+    extern const char*  bluetooth_icon_png;
+    const int           bluetooth_icon_pngSize = 12807;
+
+    extern const char*  wifi_icon_png;
+    const int           wifi_icon_pngSize = 15341;
+
+}
