d39adc33ddc84096b2fbc2ffa1e4692bbf6cb8bf
Dylan Marcus <dylan@dylanjmarcus.com>
2024-09-18 02:17:40 -0400

    update renamed vars

--- UI/M1OrientationClientDropdownMenu.h
+++ UI/M1OrientationClientDropdownMenu.h
@@ -166,7 +166,7 @@ public:
     bool opened = false;
     int selectedOption = 0;
     std::vector<std::string> options;
-    Mach1DecodeAlgoType* dataToControl = nullptr;
+    Mach1DecodeMode* dataToControl = nullptr;
     
     int optionHeight = 30;
     int fontSize = DEFAULT_FONT_SIZE;
@@ -181,7 +181,7 @@ public:
     MurkaShape triggerButtonShape;
     TextAlignment textAlignment = TEXT_CENTER;
 
-    M1OrientationClientDropdownMenu & controlling(Mach1DecodeAlgoType* dataPointer) {
+    M1OrientationClientDropdownMenu & controlling(Mach1DecodeMode* dataPointer) {
         dataToControl = dataPointer;
         return *this;
     }
